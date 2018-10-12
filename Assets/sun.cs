82fa1160db608aa0d59ce02c207bb0aea731dc5a
uesugiyuam <tsar84209@gmail.com>
2018-10-12 08:53:53 +0800

    Initial commit

---
+++ Assets/sun.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sun : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform tramsform = GetComponent<Transform>();
+        Vector3 angle = new Vector3(0,2.0f, 0);
+        transform.Rotate(angle);
+	}
+}
